949a209a0381f55041dc2b5c156572e5c196de65
S. Scott Collis <sscollis@me.com>
2022-05-31 17:23:07 -0600

    Added CF test case

---
+++ thesis/CFtest/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+../../../fsc/fsc < fsc.inp
+tail -n +2 cprofile.dat > profile.0
+tail -n +2 cfirst.dat > first.0
+tail -n +2 csecond.dat > second.0
+#
+# Temporal problem
+#
+../../stab < temporal.inp
+../../getevec << EOF
+evec.dat
+480
+0
+EOF
+#
+# Spatial problem
+#
+#../stab < spatial.inp
+#../getevec << EOF
+#evec.dat
+#371
+#0
+#EOF
+#
+# Shooting to cleanup spatial 
+#
+#../../shoot/shoot < shoot.inp
+#
+exit $?
